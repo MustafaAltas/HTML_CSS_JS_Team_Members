e93367021a5daf288dba83f75a92ce5dacc240fa
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-03-04 06:28:42 +0300

    template hazırlandı js dosyası son haline getirildi.

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-window.addEventListener("load" ,() => {
-    let düğme = document.getElementById("flexSwitchCheckDefault");
+window.addEventListener("load", () => {
+  document.querySelectorAll(".kart").forEach((e) => {
+    e.style.backgroundColor = "#f4f4f4";
+  });
+  document.querySelector("body").style.backgroundColor = "#f4f4f4";
+  let düğme = document.getElementById("flexSwitchCheckDefault");
 
-    düğme.addEventListener("click",(e) => {
-        console.log(e.target.value);
-        e.target.toggleAttribute("checked");
-        if (düğme.hasAttribute("checked")) {
-            document.querySelector("body").style.backgroundColor="black";
-        }else {
-            document.querySelector("body").style.backgroundColor="white";
-        }
-    })
-})
-
+  düğme.addEventListener("click", (e) => {
+    console.log(e.target.value);
+    e.target.toggleAttribute("checked");
+    if (düğme.hasAttribute("checked")) {
+      document.querySelector("body").style.backgroundColor = "#131414";
+      document.querySelector("body").style.color = "white";
+      document.querySelectorAll(".kart").forEach((e) => {
+        e.style.Color = "white";
+        e.style.backgroundColor = "#131414";
+      });
+    } else {
+      document.querySelector("body").style.backgroundColor = "#f4f4f4";
+      document.querySelector("body").style.color = "black";
+      document.querySelectorAll(".kart").forEach((e) => {
+        e.style.Color = "black";
+        e.style.backgroundColor = "#f4f4f4";
+      });
+    }
+  });
+});
